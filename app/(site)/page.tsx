57479ea9020773c08ece32fec3a5c7528c5360b4
eunhye <pehwish@gmail.com>
2024-08-15 13:29:36 +0900

    chapter 4 진행중

---
+++ app/(site)/page.tsx
@@ -0,0 +1,11 @@
+import { sleep } from '@/lib/utils';
+import React from 'react';
+
+const page = () => {
+  // await sleep(2000);
+
+  // throw new Error('my error');
+  return <div>Home Page</div>;
+};
+
+export default page;
